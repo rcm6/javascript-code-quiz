d87b242297b9aa1bacf352eece53b3ac8ec3ba97
Russell Coleman <rootofsilence@gmail.com>
2023-01-16 13:02:42 +0000

    added initial questions/answers for testing

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+      question: "what:",
+      choices: ["steak", "pizza", "chips", "cake"],
+      answer: "cake",
+    },
+    {
+      question: "why",
+      choices: ["because", "why not", "well", "shrug"],
+      answer: "because",
+    },
+    {
+      question: "when",
+      choices: ["now", "then", "never", "all of the above",],
+      answer: "all of the above",
+    },
+    {
+      question: "where",
+      choices: ["home", "away", "left", "right"],
+      answer: "right",
+    },
+    {
+      question: "who",
+      choices: ["me", "you", "they", "it"],
+      answer: "they",
+    },
+  ];
